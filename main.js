a211e36642492e6734cef1799f31b680ba7fc1ca
Vicram Vijayakumar <vicram.vijayakumar@gmail.com>
2022-10-15 23:20:19 -0700

    Updates Console + Alerts to User

---
+++ main.js
@@ -0,0 +1,21 @@
+function submission_confirmation(e) {
+    e.preventDefault();
+    // TODO: Fill in the rest 
+
+
+    var task = document.getElementById("task").value
+    var priority = document.getElementById("priority").value
+    var day = document.getElementById("day").value
+    var time = document.getElementById("time").value
+
+
+    console.log("task: " + task)
+    console.log("priortiy: " + priority)
+    console.log("day: " + day)
+    console.log("time: " + time)
+
+    alert("Task has been sucessfully added.")
+
+
+
+}
