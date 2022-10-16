c0607ccce2b173744bee3cc8ea04cb5b0f38eb93
Joseph Liu <joseph.liu.927@berkeley.edu>
2022-10-16 09:11:40 -0700

    Revert "fixed cookies expiration bug"
    This reverts commit f790baf9290531d3ca88c29150e2de527c65c864.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const cookieStorage = {
 const storageType = cookieStorage;
 function createTaskCookie(task, priority, day, time) {
     var date = new Date();
-    date.setTime(date.getTime()+ (90*24*60*60*1000));
+    date.setTime(date.getTime()+ (expiry*24*60*60*1000));
     var expires = "expires=" + date.toGMTString();
     document.cookie = task + "=" + priority + "/" + day + "/" + time + ";" + expires;
     //cookieStorage.setItem(task, value);
