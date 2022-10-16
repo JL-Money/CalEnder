41288249f1387e06d679a82a4b02e987c1b18199
Joseph Liu <joseph.liu.927@berkeley.edu>
2022-10-16 11:57:00 -0700

    Revert "try to debug js"
    This reverts commit 96a00efc8bf9d1655822965a5227538780b88d43.

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function runApp() {
         let start_time = parseInt(time);
         let end_time = parseInt(time.split('-')[1]);
 
-        for (let r = start_time; r < end_time; r++) {
+        for (let r = start_time; r <= end_time; r++) {
             let row = document.getElementById(r.toString());
             row.cells[column].innerHTML = task;
         }
